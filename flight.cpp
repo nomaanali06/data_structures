cd889eddd289306889843f2b70b314d3a53103a2
Noman Ali <97123202+nomaanali06@users.noreply.github.com>
2024-10-05 00:35:47 -0700

    Corrected indentation

--- flight.cpp
+++ flight.cpp
@@ -5,17 +5,20 @@
 
 using namespace std;
 
+// FlightReservationSystem class encapsulates all flight reservation functionalities
 class FlightReservationSystem {
+    // Node structure representing each destination and flight information
     struct Node {
-        string name;
-        string date;
-        string departure_time;
-        string arrival_time;
-        string cost;
-        string flight;
-        string hotelfair;
-        Node *next;
+        string name;               // Destination or source name
+        string date;               // Date of the flight
+        string departure_time;     // Departure time of the flight
+        string arrival_time;       // Arrival time of the flight
+        string cost;               // Cost of the flight
+        string flight;             // Flight name
+        string hotelfair;          // Hotel charges per day at destination
+        Node *next;                // Pointer to the next destination node
 
+        // Constructor for source node
         Node(string source) {
             name = source;
             next = nullptr;
@@ -27,6 +30,7 @@ class FlightReservationSystem {
             hotelfair = "";
         }
 
+        // Constructor for destination node with flight information
         Node(string destination, string dte, string d_t, string a_t, string cst, string flt) {
             name = destination;
             date = dte;
@@ -38,10 +42,12 @@ class FlightReservationSystem {
         }
     };
 
+    // Array to store source nodes (with linked destinations)
     int index;
     Node* array[10];
 
 public:
+    // Constructor to initialize the array and index
     FlightReservationSystem() {
         index = 0;
         for (int i = 0; i < 10; i++) {
@@ -49,6 +55,7 @@ public:
         }
     }
 
+    // Search for a source node by name and return its index, -1 if not found
     int search(string src) {
         for (int i = 0; i < index; i++) {
             if (array[i]->name == src)
@@ -57,6 +64,7 @@ public:
         return -1;
     }
 
+    // Insert a source node if it doesn't already exist
     void sourceInsert(string abc) {
         if (index == 0) {
             array[index] = new Node(abc);
@@ -70,6 +78,7 @@ public:
         }
     }
 
+    // Insert a destination node in the linked list of a source node
     void insert(int indx, Node* nd) {
         if (array[indx]->next == nullptr) {
             array[indx]->next = nd;
@@ -82,6 +91,7 @@ public:
         }
     }
 
+    // Convert string to integer (for cost comparison)
     int stringToInt(string s) {
         stringstream geek(s);
         int x = 0;
@@ -89,6 +99,7 @@ public:
         return x;
     }
 
+    // Find the minimum price path from source to destination and store it in a file
     void minimumPricePath(string src, string dst) {
         int src_index = search(src);
         if (src_index == -1) {
@@ -102,6 +113,7 @@ public:
         while (ptr != nullptr) {
             if (ptr->name == dst) {
                 fares = stringToInt(ptr->cost);
+                // Save the flight with the highest fare
                 if (fares > temp2) {
                     ofstream myfile;
                     myfile.open("minimumfare.txt");
@@ -116,6 +128,7 @@ public:
         displayFile("minimumfare.txt");
     }
 
+    // Find the minimum time path from source to destination and store it in a file
     void minimumTimePath(string src, string dst) {
         int src_index = search(src);
         if (src_index == -1) {
@@ -129,6 +142,7 @@ public:
         while (ptr != nullptr) {
             if (ptr->name == dst) {
                 fares = stringToInt(ptr->cost);
+                // Save the flight with the shortest duration (lowest cost assumed to represent time here)
                 if (fares < temp2) {
                     ofstream myfile;
                     myfile.open("minimumtime.txt");
@@ -143,6 +157,7 @@ public:
         displayFile("minimumtime.txt");
     }
 
+    // Display all flights between a source and destination
     void path(string src, string dst) {
         int src_index = search(src);
         if (src_index == -1) {
@@ -160,6 +175,7 @@ public:
         }
     }
 
+    // Display flights on a specific date between a source and destination
     void datePath(string src, string dst, string dte) {
         int src_index = search(src);
         if (src_index == -1) {
@@ -183,6 +199,7 @@ public:
         }
     }
 
+    // Display flights from a specific airline between a source and destination
     void flightPath(string src, string dst, string flt) {
         int src_index = search(src);
         if (src_index == -1) {
@@ -206,6 +223,7 @@ public:
         }
     }
 
+    // Display all direct flights from each source with their hotel charges
     void displayDirectFlights() {
         for (int i = 0; i < index; i++) {
             cout << "[Source] " << array[i]->name << " : Hotel fair/day = " << array[i]->hotelfair << endl;
@@ -219,54 +237,51 @@ public:
         }
     }
 
+    // Load flight data from a file and populate the flight reservation system
     void loadFlightData(const string& filename) {
         ifstream in(filename);
         string input;
         while (getline(in, input)) {
             istringstream iss(input);
-            string word[7];
-            for (int i = 0; i < 7; i++) {
-                iss >> word[i];
-            }
-            sourceInsert(word[0]);
-            Node* newnode = new Node(word[1], word[2], word[3], word[4], word[5], word[6]);
-            insert(search(word[0]), newnode);
+            string src, dst, dte, d_t, a_t, cst, flt;
+            iss >> src >> dst >> dte >> d_t >> a_t >> cst >> flt;
+
+            // Insert source and destination nodes
+            sourceInsert(src);
+            int index = search(src);
+            Node* nd = new Node(dst, dte, d_t, a_t, cst, flt);
+            insert(index, nd);
         }
         in.close();
     }
 
-    void loadHotelCharges(const string& filename) {
-        ifstream in(filename);
-        string input;
-        while (getline(in, input)) {
-            istringstream iss(input);
-            string word[2];
-            for (int i = 0; i < 2; i++) {
-                iss >> word[i];
-            }
-            if (search(word[0]) != -1) {
-                array[search(word[0])]->hotelfair = word[1];
-            }
-        }
-        in.close();
-    }
-
-private:
+    // Helper function to display contents of a file
     void displayFile(const string& filename) {
-        ifstream in(filename);
-        string input;
-        while (getline(in, input)) {
-            cout << input << endl;
+        ifstream myfile(filename);
+        string line;
+        if (myfile.is_open()) {
+            while (getline(myfile, line)) {
+                cout << line << endl;
+            }
+            myfile.close();
         }
-        in.close();
     }
 };
 
+// Main function to demonstrate the flight reservation system
 int main() {
     FlightReservationSystem system;
 
-    system.loadFlightData("Flights.txt");
-    system.loadHotelCharges("HotelCharges_perday.txt");
+    // Load flight data from a file
+    system.loadFlightData("flightdata.txt");
 
-    cout << "************ FLIGHT RESERVATION SYSTEM ************" << endl;
+    // Example function calls
+    system.minimumPricePath("NY", "LA");
+    system.minimumTimePath("NY", "LA");
+    system.path("NY", "LA");
+    system.datePath("NY", "LA", "12-12-2024");
+    system.flightPath("NY", "LA", "Delta");
+    system.displayDirectFlights();
 
+    return 0;
+}
